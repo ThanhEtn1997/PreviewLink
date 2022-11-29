0ed0f09d85e345cdcd2dee4fa3000eb573865d7e
ThanhEtn <thanh,nguyenvan@pharmapoc.com>
2022-11-29 13:48:20 +0700

    add env config

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require('express');
 fs = require('fs');
+require('dotenv').config();
 
 const app = express()
-const PORT = 3000
+const PORT = process.env.PORT || 3000
 
 app.use(express.static('media'));
 
